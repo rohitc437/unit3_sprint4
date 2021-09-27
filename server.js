829d29a24e6b9e525ad3fea6fdec2bdaa64fc937
Rohit <rohitc437>
2021-09-27 15:20:02 +0530

    updated 1

---
+++ server.js
@@ -0,0 +1,78 @@
+
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const connect = () => {
+    return mongoose.connect("mongodb://127.0.0.1:27017/naukri")
+}
+
+const app = express();
+app.use(express.json())
+
+//step1: creating schema for jobs 
+const jobSchema = new mongoose.Schema({
+    id:{type:Number, required:true},
+    job_name:{type:String, required:true},
+    skill:{type:String, required:true},
+    city:{type:String, required:true},
+    rating:{type:Number, required:true}
+})
+
+//step2: create model for schema
+const Job = mongoose.model("job", jobSchema)
+
+//api for find all job
+app.get("/jobs", async ( req, res) => {
+    const jobs = await Job.find()
+
+    return res.status(200).send({jobs})
+})
+
+//find job perticular city and skill
+app.get("/jobs/Barajalan&RecruitingManager", async ( req, res) => {
+    const job1 = await Job.find({city:"Barajalan"})
+
+    const job2 = await Job.find({$skill:"Recruiting Manager"})
+    return res.status(200).send({job1,job2})
+})
+
+//sorting job by rating
+app.get("/jobs/sortJob", async ( req, res) => {
+    const jobs = await Job.find().sort({rating:-1})
+
+    return res.status(200).send({jobs})
+})
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(2345, async function(req, res){
+    await connect();
+    console.log("Jobs search engine started")
+})
